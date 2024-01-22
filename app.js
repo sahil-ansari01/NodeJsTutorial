fb84c48fa274c26bc2e6a14231ddbec63dc2bdbe
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-01-22 11:22:14 +0530

    request/response

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.setHeader('Content-Type', 'text/html'); 
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello form my Nodejs server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const routes = require('./routes');
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
  
 server.listen(3000);    
