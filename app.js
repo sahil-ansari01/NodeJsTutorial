bc0d2e2af3a1bcb7482409a13d91ad6c52aa538b
sahil-ansari01 <opensourcecarrier@gmail.com>
2024-01-26 11:03:50 +0530

    added express to project

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes.handler);
- 
-server.listen(3000);    
+app.use( (req, res, next) => {
+    console.log("In the middleware!");
+    next();  // this allows the code to pass to next line/content of this page
+})
+
+app.use( (req, res, next) => {
+    console.log("In another the middleware!");
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000);
